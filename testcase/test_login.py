95073c66e2ba6f79a972d8c79f55a73a2a8bc8df
Ljianye <1762696875@qq.com>
2021-10-21 10:05:39 +0800

    66

--- testcase/test_login.py
+++ testcase/test_login.py
@@ -10,12 +10,19 @@ class TestLogin():
         # 第一次实例化
         self.loginpage = LoginPage()
 
-    @pytest.mark.parametrize("data",GetYaml("../Data/logindata.yml").get_data("正确数据"))
-    def test_login(self,data:dict):
-        #self.loginpage.open("https://test.admin.mokkaya.com/#/login")
-        print(data)
-        print(type(data))
-        # self.loginpage.inputname("邮箱",data["name"])
-        # self.loginpage.inputpwd("密码", data["pwd"])
-        # self.loginpage.clicklogin("登录")
+    @pytest.mark.parametrize("data",GetYaml("../Data/logindata.yml").get_data("success"))
+    def test_login(self,data):
+        name=data["name"]
+        pwd=data["pwd"]
+        self.loginpage.open("https://test.admin.mokkaya.com/#/login")
+        self.loginpage.login(name,pwd)
 
+
+    @pytest.mark.parametrize("data", GetYaml("../Data/logindata.yml").get_data("faile"))
+    def test_filelogin(self, data):
+        name = data["name"]
+        pwd = data["pwd"]
+        self.loginpage.open("https://test.admin.mokkaya.com/#/login")
+        self.loginpage.inputname("邮箱", name)
+        self.loginpage.inputpwd("密码", pwd)
+        self.loginpage.clicklogin("登录")
